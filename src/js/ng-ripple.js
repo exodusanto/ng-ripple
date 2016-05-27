394248d715f156cd8f9e0b54737e11a6db920c99
Antonio Dal Sie <antonydalsie@gmail.com>
2016-05-27 13:06:09 +0200

    Update standalone

--- src/js/ng-ripple.js
+++ src/js/ng-ripple.js
@@ -13,46 +13,61 @@
 	ripple.directive('ripple',['rippleConfig', function(rippleConfig){
 
 		function rippleInit(scope,element,attributes){
-			var elem = null;
 			var rippleCont = null;
 			var inkLight = false;
 			var inkColor = false;
 			var customOpacity = null;
 			var icon = false;
-			
-			elem = element[0];
-			rippleCont = elem.querySelectorAll(".ink-content")[0];
+			var overInk = false;
+		
+			removeClass(element,'ripple');
+			rippleCont = element[0].querySelectorAll(".ink-content")[0];
 
 			element.on("$destroy",function(){
-				elem.removeEventListener('mousedown',createRipple);
+				removeListenerMulti(element[0],"mousedown touchstart",createRipple);
 			});
 
-			icon = hasClass(elem,'r-icon');
+			icon = hasClass(element,'r-icon');
+			overInk = hasClass(element,'r-overink');
 			inkLight = typeof attributes.rLight !== "undefined";
 			inkColor = typeof attributes.rColor !== "undefined" ? attributes.rColor : false;
 			customOpacity = typeof attributes.rOpacity !== "undefined" ? attributes.rOpacity : null;
 
-			if(typeof attributes.rDisabled == "undefined" && !hasClass(elem,'disabled')){
-				elem.addEventListener('mousedown',createRipple);
+			if(typeof attributes.rDisabled == "undefined" && !hasClass(element,'disabled')){
+				addListenerMulti(element[0],"mousedown touchstart",createRipple);
 			}
 
 
 			function createRipple(event){
+				var targetInk = $(event.target);
+
+				if(hasClass(targetInk,'r-noink') || !!parents(targetInk,'.r-noink').length)return;
+
+				if(!!overInk)rippleCont.style.display = "block";
+
+
 				var ink = document.createElement("i");
 				var incr = 0;
 
 				addClass(ink,'ink');
 
+				removeClass(rippleCont.querySelectorAll(".ink"),'new');
+				addClass(ink,'new');
+
 				rippleCont.insertBefore(ink,rippleCont.firstChild);
 				
 				var d = Math.max(rippleCont.offsetWidth, rippleCont.offsetHeight);
 				
-				ink.style.height = d/2;
-				ink.style.width = d/2;
-				
-				
-				var x = event.pageX - offestElm(rippleCont).left;
-				var y = event.pageY - offestElm(rippleCont).top;
+				ink.style.height = d/2+"px";
+				ink.style.width = d/2+"px";
+
+				var x = event.type != "touchstart" ? 
+					event.pageX - offestElm(rippleCont).left : 
+					event.originalEvent.touches[0].pageX - offestElm(rippleCont).left;
+
+				var y = event.type != "touchstart" ? 
+					event.pageY - offestElm(rippleCont).top :
+					event.originalEvent.touches[0].pageY - offestElm(rippleCont).top;
 				
 				
 				if(!icon){
@@ -64,8 +79,12 @@
 
 				if(!!inkColor){
 					ink.style.backgroundColor = inkColor;
+
+					var rgba = hexToRGB(inkColor);
+					rippleCont.style.backgroundColor = 'rgba('+rgba.r+','+rgba.g+','+rgba.b+','+.098+')';
 				}else if(!!inkLight){
 					ink.style.backgroundColor = "rgb(255,255,255)";
+					rippleCont.style.backgroundColor = 'rgba(255,255,255,'+.098+')';
 				}
 
 				addClass(ink,'animate');
@@ -95,34 +114,92 @@
 						},50);
 					},100);
 				}
+
+				function listenerPress(){
+					addListenerMulti(window,'mouseup blur touchend',removeInk);
+					addListenerMulti(element[0],'mouseleave',removeInk);
+				}
 				
 				function removeInk(){
-					addListenerMulti(window,'mouseup mouseleave blur',function(){
-						removeListenerMulti(this,'mouseup mouseleave blur',this);
+					removeListenerMulti(window,'mouseup mouseleave touchend',removeInk);
+					removeListenerMulti(element[0],'mouseleave',removeInk);
 
-						clearInterval(inkGrow);
+					clearInterval(inkGrow);
 
+					var delay = incr < 2 && element.prop('nodeName').toLowerCase() == 'a' ? 100 : 1;
+					incr = incr < 2 ? 2 : incr += .5;
+					setTimeout(function(){
+						ink.style.height = d*incr+"px",
+						ink.style.width = d*incr+"px",
+						ink.style.opacity = 0
+
+						if(!!hasClass(ink,'new'))rippleCont.style.backgroundColor = "";
 						setTimeout(function(){
-
-							ink.style.opacity = 0;
-
-							setTimeout(function(){
-								ink.remove();
-							},550);
-						},100);
-					});
+							ink.remove();
+							if(!!overInk && !rippleCont.querySelectorAll(".ink").length)rippleCont.style.display = "none";
+						},650);
+					},delay);
 				}
 
 				hoverIncrement();
-				removeInk();
+				listenerPress();
 			}
 		}
 
-		function addClass(e,name){
-			if (e.classList){
-			  e.classList.add(name);
-			}else{
-			  e.className += ' ' + name;
+		function parents(el,sl){
+			var parents = [];
+
+			var p = el[0].parentNode;
+
+			while(p !== null){
+				var o = p;
+
+				if(hasClass(o,sl))parents.push(o);
+				p = o.parentNode;
+			}
+			return parents;
+		}
+
+		function addClass(el,name){
+			var ex = typeof el.length != "undefined" ? el.length > 0 : !!el && el != [];
+			if(ex && el != []){
+				if(Array.isArray(el)){
+					el.forEach(function(e){
+						if (e.classList){
+						  e.classList.add(name);
+						}else{
+						  e.className += ' ' + name;
+						}
+					});
+				}else{
+					if (el.classList){
+					  el.classList.add(name);
+					}else{
+					  el.className += ' ' + name;
+					}
+				}
+			}
+		}
+
+		function removeClass(el,name){
+			var ex = typeof el.length != "undefined" ? el.length > 0 : !!el && el != [];
+			if(ex){
+				if(Array.isArray(el)){
+					el.forEach(function(e){
+						if (e.classList){
+						  e.classList.remove(name);
+						}else{
+						  e.className = e.className.replace(new RegExp("/("+name+")\s/"),"");
+						}
+					});
+				}else{
+					el = el[0];
+					if (el.classList){
+					  el.classList.remove(name);
+					}else{
+					  el.className = el.className.replace(new RegExp("/("+name+")\s/"),"");
+					}
+				}
 			}
 		}
 
@@ -157,6 +234,17 @@
 		  }
 		}
 
+		function hexToRGB(hex){
+
+			var patt = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
+			var matches = patt.exec(hex);
+
+			return {
+				r:parseInt(matches[1], 16),
+				g:parseInt(matches[2], 16),
+				b:parseInt(matches[3], 16)
+			};
+		}
 
 		function createMarkup(element){
 			var content = element[0].innerHTML;
